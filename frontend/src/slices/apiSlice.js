ff7720b823d70702cd59fb26ec4c001babe5c90f
OlawaleP <99323873+OlawaleP@users.noreply.github.com>
2024-12-19 11:46:55 +0100

    remove from cart

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,28 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { BASE_URL } from '../constants';
 
-const initialState = {
-  userInfo: localStorage.getItem('userInfo')
-    ? JSON.parse(localStorage.getItem('userInfo'))
-    : null,
-};
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
-const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {
-    setCredentials: (state, action) => {
-      state.userInfo = action.payload;
-      localStorage.setItem('userInfo', JSON.stringify(action.payload));
-    },
-    logout: (state, action) => {
-      state.userInfo = null;
-      // NOTE: here we need to also remove the cart from storage so the next
-      // logged in user doesn't inherit the previous users cart and shipping
-      localStorage.clear();
-    },
-  },
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ['Product', 'Order', 'User'],
+  endpoints: (builder) => ({})
 });
-
-export const { setCredentials, logout } = authSlice.actions;
-
-export default authSlice.reducer;
