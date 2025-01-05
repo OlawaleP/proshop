f4132bb76d2c9e43a35de78c63edc2fb06d5139c
OlawaleP <99323873+OlawaleP@users.noreply.github.com>
2025-01-05 16:22:31 +0100

    get user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,7 +15,7 @@ const authUser = asyncHandler(async (req, res) => {
 
         generateToken(res, user._id);
 
-        res.json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -77,7 +77,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile');
+    const user = await user.findById(req.user._id);
+
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 }); 
 
 // @desc    Update user profile
